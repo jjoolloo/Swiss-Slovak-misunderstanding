457f740055ff9c793225aec354ef92aeb3febd28
Alexander Joehl <joehla@ethz.ch>
2011-11-07 17:12:01 +0100

    first structs done

---
+++ Persons_struct.m
@@ -0,0 +1,22 @@
+
+%exact coordinates
+person.ex_x = [1 1 1];
+person.ex_y = [1 2 3];
+
+%on what cell are they standing
+person.int_x = [1 1 1];
+person.int_y = [1 2 3];
+
+%on what floor are they
+person.level = [1 1 1];
+
+
+person.mass = [2 2 2];
+
+%force acting on the person
+person.force_x = [0 0 0];
+person.force_y = [0 0 0];
+
+
+
+person.color = [13 31 31];
