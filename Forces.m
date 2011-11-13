576b581b1bc5164108f52e3530f906678cac5620
Alexander Joehl <alexanderjoehl@vpn-global-dhcp1-102.ethz.ch>
2011-11-13 15:28:52 +0100

    move.m done

--- Forces.m
+++ Forces.m
@@ -8,16 +8,21 @@ for i=1:person.length
     indy = person.int_y(i);
     map_of_floor = map(floor).wall + map(floor).pot;
     
+    %force by potential
     person.force_x(i) = 0.5*((map_of_floor(indy,indx-1) - map_of_floor(indy,indx+1)));
     person.force_y(i) = 0.5*((map_of_floor(indy-1,indx) - map_of_floor(indy+1,indx)));
     
-    %influence of other persons
+    %force by other persons
     for k=1:person.length
-        if k~=i
+        if k~=i && person.level(i) == person.level(k)   %not itself and only persons on the same floor
             dist = sqrt((person.ex_x(i) - person.ex_x(k))^2 + (person.ex_y(i) - person.ex_y(k))^2);
-            f = 1/dist;  %function to be adjusted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            person.force_x(i) = f*(person.ex_x(i) - person.ex_x(k))/dist;
-            person.force_y(i) = f*(person.ex_y(i) - person.ex_y(k))/dist;
+            if dist < 10 %cutoff length (1 equals 0.5 meter)
+                f = 1/dist;  %function to be adjusted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                person.force_x(i) = f*(person.ex_x(i) - person.ex_x(k))/dist;
+                person.force_y(i) = f*(person.ex_y(i) - person.ex_y(k))/dist;
+            end
          end
     end
 end
+
+clear dist f indx indy i k map_of_floor floor
--- move.m
+++ move.m
@@ -1,1 +1,18 @@
 %move them
+
+dt = 1; %timestep 
+
+for i=1:person.length
+    %update the positions
+    person.ex_x(i) = person.ex_x(i) + dt*person.force_x(i);
+    person.ex_y(i) = person.ex_y(i) + dt*person.force_y(i);
+    
+    %update the cell positions
+    person.int_x(i) = int64(person.ex_x(i));
+    person.int_y(i) = int64(person.ex_y(i));
+    
+    %reset forces
+    person.force_x(i) = 0;
+    person.force_y(i) = 0;
+    
+end
