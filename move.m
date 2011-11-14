fbc7e49dae389bdd8c1917a8194a6c6b46871c2c
Alexander Joehl <joehla@ethz.ch>
2011-11-14 16:53:55 +0100

    some working

--- move.m
+++ move.m
@@ -4,12 +4,24 @@ dt = 1; %timestep
 
 for i=1:person.length
     %update the positions
-    person.ex_x(i) = person.ex_x(i) + dt*person.force_x(i);
-    person.ex_y(i) = person.ex_y(i) + dt*person.force_y(i);
+    ex_x = person.ex_x(i) + dt*person.force_x(i);
+    ex_y = person.ex_y(i) + dt*person.force_y(i);
+    
+    int_x = int64(person.ex_x(i));
+    int_y = int64(person.ex_y(i));
+    
+    if map.wall(int_y,int_x) > 0
+        
+    else
+        person.ex_x(i)=ex_x;
+        person.ex_y(i)=ex_y;
+        person.int_x(i)=int_x;
+        person.int_y(i)=int_y;
+    end
+    
     
     %update the cell positions
-    person.int_x(i) = int64(person.ex_x(i));
-    person.int_y(i) = int64(person.ex_y(i));
+    
     
     %reset forces
     person.force_x(i) = 0;
